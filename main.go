d711d405b1ae45266cf5de9823a4c784f4a4d89b
kota oue <kotaoue@gmail.com>
2024-06-24 22:28:28 +0900

    pkgに処理を切り出して整理

--- main.go
+++ main.go
@@ -1,123 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"github.com/kotaoue/gomodoro/pkg/sound"
+	"github.com/kotaoue/gomodoro/pkg/pomodoro"
 )
 
-type config struct {
-	WindowWidth  float32
-	WindowHeight float32
-	WindowTitle  string
-	TimerLength  int
-	StartText    string
-	StopText     string
-	StopSound    string
-}
-
-var cfg = config{
-	WindowWidth:  100,
-	WindowHeight: 20,
-	WindowTitle:  "🍅",
-	TimerLength:  25 * 60,
-	StartText:    "▶",
-	StopText:     "⏹️",
-	StopSound:    "assets/expiry.mp3",
-}
-
-type pomodoro struct {
-	Label   *widget.Label
-	Button  *widget.Button
-	Second  int
-	Ticker  *time.Ticker
-	Timer   *time.Timer
-	Stopper chan struct{}
-}
-
-func (p *pomodoro) start() {
-	p.Button.SetText(cfg.StopText)
-	p.Button.OnTapped = p.stop
-
-	p.Ticker = time.NewTicker(1 * time.Second)
-	p.Timer = time.NewTimer(time.Duration(p.Second) * time.Second)
-	p.Stopper = make(chan struct{})
-
-	start := time.Now()
-	fmt.Println("START")
-
-	go func() {
-		for {
-			select {
-			case <-p.Timer.C:
-				setText(
-					p.Label,
-					"FINISH",
-				)
-				sound.Play(cfg.StopSound)
-				p.stop()
-				return
-			case <-p.Ticker.C:
-				setText(
-					p.Label,
-					secToMD(p.Second-int(time.Since(start).Seconds())),
-				)
-			case <-p.Stopper:
-				p.Timer.Stop()
-				p.Ticker.Stop()
-				return
-			}
-		}
-	}()
-}
-
-func (p *pomodoro) stop() {
-	p.Second = cfg.TimerLength
-	setText(p.Label, secToMD(p.Second))
-	p.Label.SetText(secToMD(p.Second))
-
-	p.Button.SetText(cfg.StartText)
-	p.Button.OnTapped = p.start
-	if p.Stopper != nil {
-		close(p.Stopper)
-	}
-}
-
 func main() {
-	p := &pomodoro{}
-	p.Second = cfg.TimerLength
-	p.Second = 10
-	p.Label = widget.NewLabel(secToMD(p.Second))
-	p.Button = widget.NewButton(cfg.StartText, p.start)
+	p := pomodoro.NewPomodoro(
+		pomodoro.Config{
+			WindowWidth:  100,
+			WindowHeight: 20,
+			WindowTitle:  "🍅",
+			TimerLength:  25 * 60,
+			StartText:    "▶",
+			StopText:     "⏹️",
+			StopSound:    "assets/expiry.mp3",
+		},
+	)
 
-	w := createWindow(p)
+	w := p.CreateWindow()
 	w.ShowAndRun()
-}
-
-func createWindow(p *pomodoro) fyne.Window {
-	a := app.New()
-	w := a.NewWindow(cfg.WindowTitle)
-	w.Resize(fyne.NewSize(cfg.WindowWidth, cfg.WindowHeight))
-
-	w.SetContent(
-		container.NewVBox(
-			container.NewCenter(p.Label),
-			p.Button,
-		))
-
-	return w
-}
-
-func setText(l *widget.Label, s string) {
-	fmt.Println(s)
-	l.SetText(s)
-}
-
-func secToMD(sec int) string {
-	return fmt.Sprintf("%02d:%02d", sec/60, sec%60)
 }
---
+++ pkg/pomodoro/pomodoro.go
@@ -0,0 +1,113 @@
+package pomodoro
+
+import (
+	"fmt"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/kotaoue/gomodoro/pkg/sound"
+)
+
+type Config struct {
+	WindowWidth  float32
+	WindowHeight float32
+	WindowTitle  string
+	TimerLength  int
+	StartText    string
+	StopText     string
+	StopSound    string
+}
+
+type Pomodoro struct {
+	Label   *widget.Label
+	Button  *widget.Button
+	Second  int
+	Ticker  *time.Ticker
+	Timer   *time.Timer
+	Stopper chan struct{}
+	Config  Config
+}
+
+func NewPomodoro(cfg Config) *Pomodoro {
+	p := &Pomodoro{}
+	p.Second = cfg.TimerLength
+	p.Label = widget.NewLabel(p.secToMD(p.Second))
+	p.Button = widget.NewButton(cfg.StartText, p.start)
+	p.Config = cfg
+
+	return p
+}
+
+func (p *Pomodoro) CreateWindow() fyne.Window {
+	a := app.New()
+	w := a.NewWindow(p.Config.WindowTitle)
+	w.Resize(fyne.NewSize(p.Config.WindowWidth, p.Config.WindowHeight))
+
+	w.SetContent(
+		container.NewVBox(
+			container.NewCenter(p.Label),
+			p.Button,
+		))
+
+	return w
+}
+
+func (p *Pomodoro) start() {
+	p.Button.SetText(p.Config.StopText)
+	p.Button.OnTapped = p.stop
+
+	p.Ticker = time.NewTicker(1 * time.Second)
+	p.Timer = time.NewTimer(time.Duration(p.Second) * time.Second)
+	p.Stopper = make(chan struct{})
+
+	start := time.Now()
+	fmt.Println("START")
+
+	go func() {
+		for {
+			select {
+			case <-p.Timer.C:
+				p.setText(
+					p.Label,
+					"FINISH",
+				)
+				sound.Play(p.Config.StopSound)
+				p.stop()
+				return
+			case <-p.Ticker.C:
+				p.setText(
+					p.Label,
+					p.secToMD(p.Second-int(time.Since(start).Seconds())),
+				)
+			case <-p.Stopper:
+				p.Timer.Stop()
+				p.Ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (p *Pomodoro) stop() {
+	p.Second = p.Config.TimerLength
+	p.setText(p.Label, p.secToMD(p.Second))
+	p.Label.SetText(p.secToMD(p.Second))
+
+	p.Button.SetText(p.Config.StartText)
+	p.Button.OnTapped = p.start
+	if p.Stopper != nil {
+		close(p.Stopper)
+	}
+}
+
+func (p *Pomodoro) setText(l *widget.Label, s string) {
+	fmt.Println(s)
+	l.SetText(s)
+}
+
+func (p *Pomodoro) secToMD(sec int) string {
+	return fmt.Sprintf("%02d:%02d", sec/60, sec%60)
+}
